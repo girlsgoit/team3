7dc2c0c142507ce92bc8f2cfa35ffc935d94181b
masha <kilatmarusea@gmail.com>
2023-07-28 09:24:41 +0300

    schimarea paginilor din SiteFooter

--- fd/InkAndPages/src/router/index.js
+++ fd/InkAndPages/src/router/index.js
@@ -16,7 +16,24 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: SiteFooter
+    },
+    {
+      path: '/terms',
+      name: 'terms',
+      component: SiteFooter
+    },
+    {
+      path: '/ptivacy',
+      name: 'privacy',
+      component: SiteFooter
+
+    },
+    {
+      path: '/help',
+      name: 'help',
+      component: SiteFooter
     }
+  
   ]
 })
 
